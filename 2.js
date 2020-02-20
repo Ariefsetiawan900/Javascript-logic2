42538e85880f8c6c6af4547465663b5de723a38f
Ariefsetiawan900 <arief.setiawan8788@gmail.com>
2020-02-20 13:12:35 +0700

    update file 2.js 20/02/2020

---
+++ 2.js
@@ -0,0 +1,37 @@
+const alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+  ];
+  const command = [true];
+  
+  function print(i) {
+    if (command[0] == true) {
+      return console.log("Print alphabet:", alphabet[i]);
+    }
+  }
+  
+  print(21);
